06540fe03d1b88250ba0586aa1e0a90a0a00a448
ramprasadgoud <119599288+ramprasadgoud@users.noreply.github.com>
2025-04-26 20:08:53 +0530

    Update vite.config.js

--- front-chat/vite.config.js
+++ front-chat/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: '0.0.0.0',  // Listen on all network interfaces
+    port: 3000        // Choose your preferred port
+  }
+}
